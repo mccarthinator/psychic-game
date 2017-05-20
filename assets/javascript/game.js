8d39a343bd206c55e4d2da29cbc5434658b3b90c
mccarthinator <mccarthy.cath@gmail.com>
2017-05-20 09:28:12 -0500

    updates on saturday

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,20 @@
 //VARIABLES 
 
-  // Tally wins amount.
+  // wins amount.
     var wins = 0;
-  // Tally losses amount.
+
+  // losses amount.
     var losses = 0;
-  // Tally of possible guesses.
+
+  // num of possible guesses.
     var guesses = 9;
-  // Tally of remaing guess count.
+
+  // num of guesses left
     var guessesLeft = 9;
-  // The array of possible computer choices.
-      var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  // The array of letter choices
+    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
   // The guessed letters
     var guessedLetters = [];
     var letterToGuess = null;
@@ -23,16 +28,16 @@
   //User gets 9 guesses
   var updateguessesLeft = function() {
     
-  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
-   document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
+  //taking the HTML element and setting it equal to the guessesLeft. 
+  document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
   };
 
   var updateLetterToGuess = function() {
     this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
   };
   var updateGuessesSoFar = function() {
-    // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
-    document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+    // Here we display the user's guesses as letters separated by commas. 
+    document.querySelector('#lets').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
   };
   // Function will be called when we reset everything
   var reset = function() {
@@ -68,3 +73,4 @@ document.onkeyup = function(event) {
             alert("Not so hot."); 
             reset();
         }
+};
